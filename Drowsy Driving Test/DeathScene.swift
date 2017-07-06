d856796502d4d41cfa3c893aa05112f18e195e06
QuinnRifkin <quinn.rifkin@gmail.com>
2017-07-06 09:59:51 -0400

    death scene changes

--- Drowsy Driving Test/DeathScene.swift
+++ Drowsy Driving Test/DeathScene.swift
@@ -20,6 +20,7 @@ class DeathScene: SKScene {
     
     var timelabel : UILabel!
     var highscorelabel : UILabel!
+    var zCountLabel : UILabel!
     
     var sleepFactz = [String]()
     
@@ -29,8 +30,9 @@ class DeathScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        timelabel = UILabel(frame: CGRect(x: self.frame.width/8, y: self.frame.height/5, width: 150, height: 20))
-        highscorelabel = UILabel(frame: CGRect(x: self.frame.width/4, y: self.frame.height/5, width: 150, height: 20))
+        timelabel = UILabel(frame: CGRect(x: self.frame.width/8, y: self.frame.height/5.2, width: 150, height: 30))
+        highscorelabel = UILabel(frame: CGRect(x: self.frame.width/8, y: self.frame.height/6, width: 200, height: 30))
+        zCountLabel = UILabel(frame: CGRect(x: self.frame.width/8, y: self.frame.height/4.6, width: 150, height: 30))
         
         PlayAgainButtonNode = self.childNode(withName: "PlayAgainNode") as! SKSpriteNode
         HomeButtonNode = self.childNode(withName: "HomeNode") as! SKSpriteNode
@@ -44,24 +46,35 @@ class DeathScene: SKScene {
         fact1 = UILabel(frame: CGRect(x: 50 , y: -(factBar.position.y) - 20, width: 270, height: 150))
         
         fact1.text = fact
-        fact1.font = UIFont(name: "HelveticaNeue-Light", size: 19)
+        fact1.font = UIFont(name: "HelveticaNeue-LightItalic", size: 20)
         fact1.lineBreakMode = NSLineBreakMode.byWordWrapping
         fact1.numberOfLines = 0
         fact1.textColor = .white
         fact1.textAlignment = NSTextAlignment.center
         
+        //timelabel.frame = CGRect(x: 100, y: 125, width: 200, height: 200)
         timelabel.textAlignment = NSTextAlignment.left
         timelabel.textColor = .white
-        timelabel.text = "Time: " + String( gameScene.getFinishTime())
+        timelabel.font = UIFont(name: "HelveticaNeue" , size: 20)
+        timelabel.text = "Your Time: " + String( gameScene.getFinishTime())
         
+        //highscorelabel.frame = CGRect(x: 100, y: 100, width: 200, height: 200)
         highscorelabel.textAlignment = NSTextAlignment.left
         highscorelabel.textColor = .white
-        highscorelabel.text = "Highscore: " + String( gameScene.getHighScore())
+        highscorelabel.font = UIFont(name: "HelveticaNeue" , size: 20)
+        highscorelabel.text = "Your Highscore: " + String( gameScene.getHighScore())
+        
+        //zCountLabel.frame = CGRect(x: 100, y: 150, width: 200, height: 200)
+        zCountLabel.textAlignment = NSTextAlignment.left
+        zCountLabel.textColor = .white
+        zCountLabel.font = UIFont(name: "HelveticaNeue" , size: 20)
+        zCountLabel.text = "Your Zs: " + String( gameScene.getHighScore())
         
         let when = DispatchTime.now() + 1 // change 2 to desired number of seconds
         DispatchQueue.main.asyncAfter(deadline: when) {
             self.view?.addSubview(self.timelabel)
             self.view?.addSubview(self.highscorelabel)
+            self.view?.addSubview(self.zCountLabel)
             self.view?.addSubview(self.fact1)
         }
 
