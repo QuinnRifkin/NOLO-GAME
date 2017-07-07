cfdd6cbce8f408a911c023307d94b3ab90d6ca48
QuinnRifkin <quinn.rifkin@gmail.com>
2017-07-07 09:29:47 -0400

    bunch of small changes

--- Drowsy Driving Test/MenuScene.swift
+++ Drowsy Driving Test/MenuScene.swift
@@ -33,7 +33,7 @@ class MenuScene: SKScene {
         
         factBar = self.childNode(withName: "factBar") as! SKSpriteNode
 
-        sleepFactz = ["Sleep deprivation can result in obesity and poor diet quality.", "Sleep deprivation causes heart disease.", "Sleep deprivation increases the risk of diabetes.", "Not getting enough sleep can result in rash decision making.", "Drowsy driving can be as dangerous as drunk driving.", "Getting more sleep is proven to increase performance in school.", "Putting your phone away before bed will result in a much better sleep.", "Beauty Sleep is real; Going to bed earlier can improve your physical appearance.", "An average of 83,000 car crashes occur due to drowsy driving."]
+        sleepFactz = ["Sleep deprivation can result in obesity and poor diet quality.", "Sleep deprivation causes heart disease.", "Sleep deprivation increases the risk of diabetes.", "Not getting enough sleep can result in rash decision making.", "Drowsy driving can be as dangerous as drunk driving.", "Getting more sleep is proven to increase performance in school.", "Putting your phone away before bed will result in a much better sleep.", "Beauty Sleep is real; Going to bed earlier can improve your physical appearance.", "An average of 83,000 car crashes occur each year due to drowsy driving."]
 
         fact = (String) (sleepFactz[Int(arc4random_uniform(9))])
         
