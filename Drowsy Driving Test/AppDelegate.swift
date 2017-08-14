1b74927e14dac1cf1696a23dc7b50db2930dab32
Tannay <tannay.chandhok@gmail.com>
2017-08-14 16:48:16 -0400

    graph fix

--- Drowsy Driving Test/AppDelegate.swift
+++ Drowsy Driving Test/AppDelegate.swift
@@ -126,22 +126,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         }
     }
     
-    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
-        switch(shortcutItem){
-            case startRec:
-                print ("Started")
-                do{sleepScene.quickActionStart()}
-                catch{ print("failed :()")}
-            case stopRec:
-                print ("Stopped")
-                
-                do{sleepScene.quickActionStop()}
-                catch{ print("failed :()")}
-            default:
-                print("nothing pressed")
-        }
-    }
-    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
